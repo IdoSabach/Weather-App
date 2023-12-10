fb1b716d66ccee7a341fc1d849d696c623758716
Ido <Idosabach11@gmail.com>
2023-12-10 23:01:29 +0200

    import api

---
+++ src/weather.js
@@ -0,0 +1,14 @@
+const apiKey = "9f434f090aae3ae45437f86f5ecd2a92";
+  const apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?units=metric&q=yeruham"
+
+const getData = async () => {
+  const res = await fetch(apiUrl + `&appid=${apiKey}`);
+  let data = await res.json();
+
+  console.log(data);
+
+  console.log(data.main.temp);
+};
+
+export default getData;
