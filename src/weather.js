515ab9a4daee837966209e465a5261d1c1507fa7
Ido <Idosabach11@gmail.com>
2023-12-11 00:49:23 +0200

    add async

--- src/weather.js
+++ src/weather.js
@@ -1,14 +1,17 @@
-const apiKey = "9f434f090aae3ae45437f86f5ecd2a92";
-  const apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?units=metric&q=yeruham"
+
 
 const getData = async () => {
+  const apiKey = "9f434f090aae3ae45437f86f5ecd2a92";
+  const apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?units=metric&q=yeruham"
   const res = await fetch(apiUrl + `&appid=${apiKey}`);
   let data = await res.json();
 
   console.log(data);
 
   console.log(data.main.temp);
+
+  return data
 };
 
 export default getData;
