2637e9ff5ecb8e900cb37dd145df71f74ffb514d
Ido <Idosabach11@gmail.com>
2023-12-12 23:10:47 +0200

    add default value of input

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import getData from "./weather";
 
-getData('yeruham')
+getData('tel aviv')
 const form = document.querySelector('.input-text')
+form.value = 'Tel Aviv'
 const submitBtn = document.querySelector('.submit-btn')
 
 submitBtn.addEventListener('click',function(e){
@@ -11,3 +12,6 @@ submitBtn.addEventListener('click',function(e){
 
   form.value = ""
 })
+
+
+
